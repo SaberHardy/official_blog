c4dd4f8059c6a4239b8d336d11fc0d74efec6828
SaberHardy <samirsamado5@gmail.com>
2022-05-25 15:15:59 +0300

    upload project

---
+++ blogapp/models.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import User
+from django.utils import timezone
+from django.db import models
+
+
+class Post(models.Model):
+    options = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    publish = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    content = models.TextField()
+    status = models.CharField(max_length=10, choices=options, default='draft')
+
+    def __str__(self):
+        return self.title
