5324c47539d023735282160d7afedea0be8e5181
SaberHardy <samirsamado5@gmail.com>
2022-05-27 16:54:16 +0300

    render comments to template

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,6 +1,7 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 
+from blogapp.forms import CommentForm
 from blogapp.models import Post
 
 
@@ -13,10 +14,24 @@ def home(request):
 
 
 def single_post(request, post):
-    post_single = get_object_or_404(Post, slug=post, status='published')
+    post = get_object_or_404(Post, slug=post, status='published')
+    comments = post.comments.filter(status=True)
+    user_comment = None
+
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            user_comment = comment_form.save(commit=False)
+            user_comment.post = post
+            user_comment.save()
+            return HttpResponseRedirect('/' + post.slug)
+    else:
+        comment_form = CommentForm()
 
     context = {
-        'post_single': post_single
+        'comment_form': comment_form,
+        'post': post,
+        'comments': comments,
+        'user_comment': user_comment,
     }
     return render(request, 'blogapp/single_post.html', context)
-
