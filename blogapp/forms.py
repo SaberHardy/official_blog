203c3ffe8d0c4af36469120511d5ab7b71396388
SaberHardy <samirsamado5@gmail.com>
2022-05-29 15:26:53 +0300

    add comment using trees

--- blogapp/forms.py
+++ blogapp/forms.py
@@ -1,12 +1,23 @@
 from django import forms
+from mptt.forms import TreeNodeChoiceField
 
 from blogapp.models import Comment
 
 
 class CommentForm(forms.ModelForm):
+    parent = TreeNodeChoiceField(queryset=Comment.objects.all())
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['parent'].required = False
+        self.fields['parent'].label = ''
+        self.fields['parent'].widget.attrs.update(
+            {'class': 'd-none'}
+        )
+
     class Meta:
         model = Comment
-        fields = ('name', 'email', 'content')
+        fields = ('name', 'parent', 'email', 'content')
         widgets = {
             'name': forms.TextInput(attrs={"class": "col-sm-12"}),
             'email': forms.TextInput(attrs={"class": "col-sm-12"}),
