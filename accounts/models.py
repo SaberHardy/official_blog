fa8c11d774b25a9fe5058de0da839c4c4124a8d3
SaberHardy <samirsamado5@gmail.com>
2022-06-03 15:27:38 +0300

    use signals to link the profile-user

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 def user_directory_path(instance, filename):
@@ -13,3 +15,10 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        # after created the new user will link it to profile
+        Profile.objects.create(user=instance)
